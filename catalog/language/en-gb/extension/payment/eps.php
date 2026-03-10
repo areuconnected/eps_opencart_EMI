2c5ce32281ad35ac1e8a35e5c93a4e0f0a35036d
areuconnected <jabedshoeb@outlook.com>
2026-03-10 11:27:11 +0600

    Use image HTML for EPS title; update loading
    Replace EPS language title with an HTML snippet that displays a checkout image and hidden descriptive text, and change the loading string to 'Redirecting to secure gateway...'. This change also removes several other language entries (button label, payment success/failure, token/txid errors and callback messages) from catalog/language/en-gb/extension/payment/eps.php.

--- catalog/language/en-gb/extension/payment/eps.php
+++ catalog/language/en-gb/extension/payment/eps.php
@@ -1,16 +1,4 @@
 <?php
 // Text
-$_['text_title']             = 'EPS - Easy Payment System';
-$_['text_description']       = 'Secure payment via EPS (Bangladesh). You will be redirected to EPS payment page after confirmation.';
-$_['button_confirm']         = 'Pay with EPS';
-$_['text_loading']           = 'Loading...';
-
-// Success / Error messages
-$_['text_payment_success']   = 'Payment completed successfully!';
-$_['text_payment_failed']    = 'Payment failed or was cancelled. Please try again.';
-$_['text_token_error']       = 'Could not connect to EPS. Please try again later.';
-$_['text_txid_missing']      = 'Transaction ID not found. Please contact support.';
-
-// Callback messages
-$_['text_payment_cancelled'] = 'Payment was cancelled.';
-$_['text_payment_error']     = 'There was an error processing your payment.';
+$_['text_title'] = '<img src="image/catalog/Checkout-Pay_with_EPS.webp" alt="EPS" style="height: 60px; vertical-align: middle; margin-right: 10px;" /><span style="display:none; vertical-align: middle; font-weight: bold;">EPS - Credit/Debit Card / Mobile Banking</span>';
+$_['text_loading']     = 'Redirecting to secure gateway...';
